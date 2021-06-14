23c27bb5df6f1125aa96fbf4c9e86d09426cf679
Kim Carpico <kcarpico@gmail.com>
2021-06-14 13:27:31 -0700

    Created model and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TWEET #1
+Tweet.create({
+    title: "Just found this", 
+    content: "the square of the hypotenuse is equal to the sum of the squares of the other two sides", 
+    author: "Pythagoras570"
+})
+
+# TWEET #2
+Tweet.create({
+    title: "I'm walkin' here", 
+    content: "Hey, I'm walkin' here!", 
+    author: "Nicky62"
+})
+
+# Use Faker to generate 10 random Tweets
+10.times do
+ Tweet.create(
+     title:  Faker::Hipster.sentence(word_count: 3),
+     content: Faker::Quote.fortune_cookie,
+     author: Faker::Twitter.screen_name
+ )
+end
