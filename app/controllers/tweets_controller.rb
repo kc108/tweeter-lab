08a91a9c747e85304548a3dd489922c3d83597ae
Kim Carpico <kcarpico@gmail.com>
2021-06-14 13:52:05 -0700

    Can POST new Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,23 @@ class TweetsController < ApplicationController
         tweet = Tweet.find(params[:id])
         render(json: { tweet: tweet })
     end
+
+    def create
+        tweet = Tweet.new(tweet_params)
+
+        if tweet.save
+            render json: { tweet: tweet }
+        else
+            render(status: 422, json: { tweet: tweet, errors: tweet.errors })
+        end
+    end
+
+    #Any methods below this
+    private
+
+    def tweet_params
+        # Returns a sanitized hash of the params with nothing extra
+        params.require(:tweet).permit(:title, :content, :author)
+    end
+
 end
